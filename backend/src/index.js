7e650b154f0aa0ab7396d3542bc6a4e87d3ad9be
Akash-SinghT <sakashh165@gmail.com>
2024-11-21 20:54:08 +0530

    added channel name

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./models/db.js";
 const app = express();
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.listen(5001, () => {
+const port = process.env.PORT;
+app.listen(port, () => {
   console.log("Server is listening");
+  connectDB();
 });
